c23040f42c1d463a47583a05a3dd579d442c73ca
Jose Luis <joseluis.llorenteperales@gmail.com>
2023-03-15 10:34:26 +0100

    Modificación hecha desde GitHub

--- PrimerProyectoJava/src/HolaMundo.java
+++ PrimerProyectoJava/src/HolaMundo.java
@@ -7,7 +7,7 @@ public class HolaMundo {
 	 */
 	public static void main(String[] args) {
 		
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo desde GitHub");
 		
 		//Comentario de línea
 		
